94bb9baa1c0b1453964fba37ef8231a04f474357
Daniil <daniil.belobragin.work@gmail.com>
2020-09-20 13:45:39 +0300

    dist repo added

--- src/main.js
+++ src/main.js
@@ -3,9 +3,6 @@ import App from './App.vue'
 import Vuelidate from "vuelidate";
 
 Vue.config.devtools = true
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/user-form/' : '/'
-}
 Vue.use(Vuelidate)
 
 new Vue({
